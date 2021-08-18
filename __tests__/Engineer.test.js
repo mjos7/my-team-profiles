8bf32779fdd78fed793bd0a40efdf54fee435737
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-08-18 15:33:17 -0700

    updates to test criteria

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,14 +3,17 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object
 test('creates an Engineer object', () => {
-  const engineer = new Engineer('Mark', 90, 'markjnjoseph@gmail.com', 'mjos7');
+  const engineer = new Engineer('Mark', 77, 'mjos7@gmail.com', 'mjos7');
 
+  expect(engineer.name).toEqual(expect.any(String));
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect(engineer.email).toEqual(expect.any(String));
   expect(engineer.github).toEqual(expect.any(String));
 });
 
 // gets github from getGithub()
 test('gets engineer github value', () => {
-  const engineer = new Engineer('Mark', 90, 'markjnjoseph@gmail.com', 'mjos7');
+  const engineer = new Engineer('Mark', 77, 'mjos7@gmail.com', 'mjos7');
 
   expect(engineer.getGithub()).toEqual(
     expect.stringContaining(engineer.github.toString())
@@ -19,7 +22,7 @@ test('gets engineer github value', () => {
 
 // gets role from getRole()
 test('gets role of employee', () => {
-  const engineer = new Engineer('Mark', 90, 'markjnjoseph@gmail.com', 'mjos7');
+  const engineer = new Engineer('Mark', 77, 'mjos7@gmail.com', 'mjos7');
 
   expect(engineer.getRole()).toEqual('Engineer');
 });
