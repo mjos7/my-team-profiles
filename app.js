996432eb15fbc094e929bb6fa7a104627e25912d
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-08-18 15:48:23 -0700

    update comments

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// node modules
+// npm modules
 const fs = require('fs');
 const inquirer = require('inquirer');
 
